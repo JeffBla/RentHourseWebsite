0fc5fdb97bcf30be39016becb5bd55da3177099e
JeffBla <jeff920305@gmail.com>
2023-04-01 11:55:00 +0800

    dev1 init

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
-const Repository = require('../src/repository');
+const Repository = require("../src/repository");
 
 /* eslint-disable no-unused-vars */
 router.get('/', (req, res, next) => {
 	const repo = new Repository('Supa Testing user');
 	res.render('index', { houses: repo.getHouses() , filter_data: require('../filter_data.js')});
+});
+
+// if want to render ejs file, just code like this
+// for font-end
+router.get("/test", (req, res, next) => {
+  res.render("index", { title: "Express", name: repo.getData() });
 });
 /* eslint-disable no-unused-vars */
 router.get('/signin', (req, res, next) => {
