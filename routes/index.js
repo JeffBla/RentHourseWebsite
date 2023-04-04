706e6a78f96680cd02212f0a246debdaa57a9c0d
cccbeginner <arvin.tsai.0113@gmail.com>
2023-04-04 22:31:19 +0800

    add post request to send filter values

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,11 @@ router.get('/', (req, res, next) => {
 });
 /* eslint-disable no-unused-vars */
 
+router.post('/submit', (req, res, next) => {
+	console.log('選擇縣市：'+req.body.county);
+	console.log('選擇區域：'+req.body.district);
+	console.log('選擇型態：'+req.body.type);
+	console.log('選擇租金：'+req.body.price);
+});
+
 module.exports = router;
