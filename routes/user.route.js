a547925327ea802b84f1eacb22ab6483d05cd73f
lcfai1223 <107372228+lcfai1223@users.noreply.github.com>
2023-05-13 06:44:09 +0000

    hashing

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,11 +3,13 @@
 const express = require("express");
 const router = new express.Router();
 const houseCtrl = require("../controllers/house.controller");
+const bcrypt = require('bcrypt');
 
 /* eslint-disable no-unused-vars */
 router.route("/").get((req, res) => {
   res.send("user");
 });
+
 
 /* eslint-disable no-unused-vars */
 router
@@ -15,10 +17,19 @@ router
   .get((req, res, next) => {
     res.render("register");
   })
-  .post((req, res, next) => {
+  .post(async (req, res, next) => {
+    console.log(req.body);
     const { username, email, password, confirmedpassword } = req.body;
-    // 在這裡處理註冊表單提交的數據
-    // ...
+    if (password !== confirmedpassword) {
+      return res.status(400).json({ message: '密碼不一致' });
+    }
+
+    // 生成salt
+    const salt = await bcrypt.genSalt();
+
+    // 加密密碼
+    const encryptedPassword  = await bcrypt.hash(req.body.password, salt);
+
     res.redirect("/user/signin"); // 跳轉到signin頁面
   });
 
@@ -28,11 +39,25 @@ router
   .get((req, res, next) => {
     res.render("signin");
   })
-  .post((req, res, next) => {
+  .post(async (req, res, next) => {
+    console.log(req.body);
     const { username, email, password } = req.body;
-    // 在這裡處理註冊表單提交的數據
-    // ...
-    res.redirect("/"); // 跳轉到主頁面
+    // 在這裡從資料庫中取得salt，然後將密碼和salt進行加密
+    const user = {
+      username,
+      email,
+      password: "從資料庫中取得的加密後的密碼",
+      salt: "從資料庫中取得的salt"
+    };
+    const match = await bcrypt.compare(password, user.password);
+    // 在這裡進行登入驗證
+    if (match) {
+      // 登入成功
+      res.redirect("/");
+    } else {
+      // 登入失敗
+      res.redirect("/user/signin");
+    }
   });
 
 router.route("/test").get((req, res) => {
