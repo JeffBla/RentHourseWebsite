c5fbf261d82a20435f423291a15209246a5bab80
JeffBla <jeff920305@gmail.com>
2023-05-14 18:49:16 +0800

    fix typo

--- SQL/rentInfo/SelectRentInfo_favor.sql
+++ SQL/rentInfo/SelectRentInfo_favor.sql
@@ -12,7 +12,7 @@ WHERE
     r.house_id=h.id AND
     h.map_object_id=m.id AND
     i_cover.id=r.image_id_cover AND
-    u.id=f.user_id AND f.user_id=r.id AND
+    u.id=f.user_id AND f.rent_info_id=r.id AND
     u.id=${userId:value}
 ORDER BY ${orderRefer:raw} ${orderMode:value}
 LIMIT ${limit:value} OFFSET ${offect:value}
